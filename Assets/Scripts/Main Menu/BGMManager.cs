e16cde34d17cefacc75124611dc7b764bc5908a3
Om Mittal <om040806mittal@gmail.com>
2025-07-20 13:42:07 +0530

    Final Touches

---
+++ Assets/Scripts/Main Menu/BGMManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    public static BGMManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Keeps the BGM object alive between scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Prevents duplicates
+        }
+    }
+}
